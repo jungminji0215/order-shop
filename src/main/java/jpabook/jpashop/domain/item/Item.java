e37e69351904c82350f40667bbbf1370c25d4292
jungminji <wjdalswl0215@gmail.com>
2023-04-15 01:15:44 +0900

    상품 entity, repository

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -28,4 +29,26 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items", fetch = LAZY)
     private List<Category> categories = new ArrayList<>();
+
+    //==비즈니스 로직==//
+    // 엔티티 자체에서 해결할 수 있는 것은 엔티티 안에 비즈니스 로직을 넣는 것이 좋다.
+    // 객체 지향적이다. 데이터를 가지고 있는 곳에서 비즈니스 로직이 나가는 것이 응집도가 있다.
+
+    /**
+     * stock 증가
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * stock 감소
+     */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
